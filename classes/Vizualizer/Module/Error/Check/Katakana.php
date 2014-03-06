faaaa3770adf28cfc957b24569bd809c010e2b7c
System Administrator <root@Naohisa-Minagawa-no-MacBook-Pro-3.local>
2014-03-06 11:19:48 +0900

    add check hiragana and/or katakana

---
+++ classes/Vizualizer/Module/Error/Check/Katakana.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright (C) 2012 Vizualizer All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author    Naohisa Minagawa <info@vizualizer.jp>
+ * @copyright Copyright (c) 2010, Vizualizer
+ * @license http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
+ * @since PHP 5.3
+ * @version   1.0.0
+ */
+
+/**
+ * カタカナのみかどうかのチェックを行う。
+ *
+ * @package Vizualizer
+ * @author Naohisa Minagawa <info@vizualizer.jp>
+ */
+class Vizualizer_Module_Error_Check_Katakana extends Vizualizer_Plugin_Module
+{
+
+    function execute($params)
+    {
+        $post = Vizualizer::request();
+        if (!empty($post[$params->get("key")]) && preg_match("/^[ァ-ヶー]+$/u", $_POST[$params->get("key")]) == 0) {
+            throw new Vizualizer_Exception_Invalid($params->get("key"), $params->get("value") . $params->get("suffix", "はカタカナで入力してください。"));
+        }
+    }
+}
