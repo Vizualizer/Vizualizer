e3669100e523e3c46a25f9116059964a4f813129
Naohisa Minagawa <info@clay-system.jp>
2014-07-04 15:33:00 +0900

    #2014070401 入力内容をコピーするモジュールを追加

---
+++ classes/Vizualizer/Module/Input/Copy.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright (C) 2012 Vizualizer All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author    Naohisa Minagawa <info@vizualizer.jp>
+ * @copyright Copyright (c) 2010, Vizualizer
+ * @license http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
+ * @since PHP 5.3
+ * @version   1.0.0
+ */
+
+/**
+ * カラムの値をコピーするクラスです。
+ *
+ * @package Vizualizer
+ * @author Naohisa Minagawa <info@vizualizer.jp>
+ */
+class Vizualizer_Module_Input_Copy extends Vizualizer_Plugin_Module
+{
+
+    function execute($params)
+    {
+        if ($params->check("target") && $params->check("result")) {
+            $post = Vizualizer::request();
+            $post->set($params->get("result"), $post[$params->get("target")]);
+        }
+    }
+}
